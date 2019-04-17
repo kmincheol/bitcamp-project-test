32c6d0d9d6de164c3c5c68be4f04fb0309a0279c
Gwangho Song <gwanghosong91@gmail.com>
2019-04-17 16:19:42 +0900

    멤버서비스수정
    수정했습니다.

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -1,27 +1,24 @@
 package bitcamp.project.test;
 
 import bitcamp.project.test.service.BoardService;
+import bitcamp.project.test.service.MemberService;
 
 public class App {
   public String getGreeting() {
     return "Hello world.";
   }
   public static void main(String[] args) {
-<<<<<<< HEAD
     System.out.println("******************************");
     BoardService board = new BoardService();
     board.list();
     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     
-    Member member = MemberService();
+    MemberService member = new MemberService();
     
-=======
-    BoardService b = new BoardService();
-    b.count();
     System.out.println("-------------------");
     for (int i = 1; i < 10; i++) {
+      member.list();
       System.out.print(i +",");
     }
->>>>>>> branch 'master' of https://github.com/kmincheol/bitcamp-project-test.git
   }
 } 
