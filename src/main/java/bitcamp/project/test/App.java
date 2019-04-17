3fbc5b1031f62393179e36f0b391deff305a07aa
Gwangho Song <gwanghosong91@gmail.com>
2019-04-17 16:11:17 +0900

    테스트
    123

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -1,35 +1,17 @@
 package bitcamp.project.test;
 
 import bitcamp.project.test.service.BoardService;
-<<<<<<< HEAD
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
-=======
->>>>>>> branch 'master' of https://github.com/kmincheol/bitcamp-project-test.git
-
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        BoardService b = new BoardService();
-        b.count();
-        System.out.println("-------------------");
-        for (int i = 1; i < 10; i++) {
-          System.out.print(i +",");
-        }
-    }
-=======
 public class App {
   public String getGreeting() {
     return "Hello world.";
   }
-
   public static void main(String[] args) {
-    System.out.println("******************************");
-    BoardService board = new BoardService();
-    board.list();
-    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    BoardService b = new BoardService();
+    b.count();
+    System.out.println("-------------------");
+    for (int i = 1; i < 10; i++) {
+      System.out.print(i +",");
+    }
   }
->>>>>>> branch 'master' of https://github.com/kmincheol/bitcamp-project-test.git
 }
