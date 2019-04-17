d7e946cc8142dfd6ecf65ba5e430a08e175f1ac2
kimkiwon307 <kkwkkj@naver.com>
2019-04-17 16:04:34 +0900

    aaaa

--- src/main/java/bitcamp/project/test/service/BoardService.java
+++ src/main/java/bitcamp/project/test/service/BoardService.java
@@ -1,7 +1,15 @@
 package bitcamp.project.test.service;
 
 public class BoardService {
+<<<<<<< HEAD
+  public void count() {
+    System.out.println("숫자 카운트");
+  }
+}
+
+=======
   public void list() {
     System.out.println("게시판 목록입니다!");
   }
 }
+>>>>>>> branch 'master' of https://github.com/kmincheol/bitcamp-project-test.git
