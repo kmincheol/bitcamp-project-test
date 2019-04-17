5c9447af485feeddfb18720304c76d66debf2a9b
jeon-sangoh <fags4634@naver.com>
2019-04-17 15:56:56 +0900

    숫자 카운트

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -3,12 +3,19 @@
  */
 package bitcamp.project.test;
 
+import bitcamp.project.test.service.BoardService;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        BoardService b = new BoardService();
+        b.count();
+        System.out.println("-------------------");
+        for (int i = 1; i < 10; i++) {
+          System.out.print(i +",");
+        }
     }
 }
---
+++ src/main/java/bitcamp/project/test/service/BoardService.java
@@ -0,0 +1,8 @@
+package bitcamp.project.test.service;
+
+public class BoardService {
+  public void count() {
+    System.out.println("숫자 카운트");
+  }
+}
+
