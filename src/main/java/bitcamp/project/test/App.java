b75d767e8c4803d7ba5596f025824aaea5ab865b
kmincheol <mincheol.kmc@gmail.com>
2019-04-17 16:30:35 +0900

    fs

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -1,12 +1,8 @@
 package bitcamp.project.test;
 
 import bitcamp.project.test.service.BoardService;
-<<<<<<< HEAD
-import bitcamp.project.test.service.MemberService;
-import bitcamp.project.test.service.PhotoBoardService;
-=======
 import bitcamp.project.test.service.MemberServiceKi;
->>>>>>> branch 'master' of https://github.com/kmincheol/bitcamp-project-test.git
+import bitcamp.project.test.service.PhotoBoardService;
 
 public class App {
   public String getGreeting() {
@@ -14,34 +10,28 @@ public class App {
   }
 
   public static void main(String[] args) {
-<<<<<<< HEAD
     System.out.println("******************************");
     BoardService board = new BoardService();
     board.list();
     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
-    MemberService member = new MemberService();
+    MemberServiceKi member = new MemberServiceKi();
     member.list();
 
     PhotoBoardService photoBoard = new PhotoBoardService();
     photoBoard.list();
 
-=======
->>>>>>> branch 'master' of https://github.com/kmincheol/bitcamp-project-test.git
     BoardService b = new BoardService();
     b.count();
     System.out.println("-------------------");
     for (int i = 1; i < 10; i++) {
       System.out.print(i +",");
     }
-<<<<<<< HEAD
-=======
      
     System.out.println("-------------------");
     MemberServiceKi m = new MemberServiceKi();
     m.list();
     System.out.println("-------------------");
     
->>>>>>> branch 'master' of https://github.com/kmincheol/bitcamp-project-test.git
   }
 }
